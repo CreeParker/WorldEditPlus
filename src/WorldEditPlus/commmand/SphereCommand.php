df6a8bd65cf95ae5e4ca7b10755c43c483442c66
CreeParker <hsgw.c.aym.98@icloud.com>
2018-09-25 01:22:46 +0900

    追加

---
+++ src/WorldEditPlus/commmand/SphereCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+/** 
+ * Copyright (c) 2018 CreeParker
+ * 
+ * <English>
+ * This plugin is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * <日本語>
+ * このプラグインは、MITライセンスのもとで公開されています。
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+declare(strict_types = 1);
+
+namespace WorldEditPlus\command;
+
+use WorldEditPlus\WorldEditPlus;
+use WorldEditPlus\processing\SphereProcessing;
+use pocketmine\command\CommandSender;
+use pocketmine\level\{Position, Level};
+use pocketmine\Player;
+
+class SphereCommand extends WorldEditPlusCommand {
+
+}
